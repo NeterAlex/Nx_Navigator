a3171c7f2cbc5e9b526624536f32f5084bb3ebc6
NeterAlex <neteralex@outlook.com>
2024-05-12 15:28:33 +0800

    feat: 更改UI

--- src/components/textCard.tsx
+++ src/components/textCard.tsx
@@ -1,16 +1,14 @@
-import {Card, CardBody} from "@nextui-org/react";
+import {Card} from "@nextui-org/react";
 
 
 export default function TextCard() {
     return (
         <Card
             isBlurred
-            className="border-none bg-background/60 dark:bg-default-100/50 w-full max-w-[610px]"
+            className="border-none bg-background/60 dark:bg-default-100/50 h-auto w-full p-[16px] max-w-[610px]"
             shadow="sm"
         >
-            <CardBody>
-                👋 Hi there, I'm NeterAlex, a fullstack developer interested in music.
-            </CardBody>
+            👋 Hi there, I'm NeterAlex, a fullstack developer interested in music.
         </Card>
     )
 }
