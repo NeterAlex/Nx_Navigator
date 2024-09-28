83650264a0ae7ad623654117a19e0bc1c0e1f7db
NeterAlex <neteralex@outlook.com>
2024-09-28 18:43:59 +0800

    feat: 调整文案与链接

--- src/components/contentCard.tsx
+++ src/components/contentCard.tsx
@@ -1,7 +1,14 @@
 import {Card, CardBody, Image, Listbox, ListboxItem, ListboxSection} from "@nextui-org/react";
 import {ReactNode} from "react";
-import {BsFillFileRichtextFill, BsFillJournalBookmarkFill, BsGithub} from "react-icons/bs";
-import {AiFillMail} from "react-icons/ai";
+import {
+    BsCodeSlash,
+    BsFillJournalBookmarkFill,
+    BsGithub,
+    BsGrid,
+    BsLaptop,
+    BsMusicPlayer,
+    BsStack
+} from "react-icons/bs";
 
 export const ListboxWrapper = ({children}: { children: ReactNode }) => (
     <div
@@ -21,6 +28,15 @@ export default function ContentCard() {
             <CardBody>
                 <ListboxWrapper>
                     <Listbox variant="flat" aria-label="Listbox menu with sections">
+                        <ListboxSection title="Intro" showDivider>
+                            <ListboxItem className={"cursor-default"}
+                                         key="fav"
+                                         description="语言学, 音乐, 小提琴、二胡演奏, 下厨, 偶尔绘画"
+                                         startContent={<BsMusicPlayer size={22}/>}
+                            >
+                                爱好
+                            </ListboxItem>
+                        </ListboxSection>
                         <ListboxSection title="Personal" showDivider>
                             <ListboxItem
                                 key="blog"
@@ -28,7 +44,7 @@ export default function ContentCard() {
                                 onClick={() => window.open("https://blog.neteralex.cn", "_blank")}
                                 startContent={<BsFillJournalBookmarkFill size={22}/>}
                             >
-                                Blog Refined
+                                Blog
                             </ListboxItem>
                             <ListboxItem
                                 key="github"
@@ -38,28 +54,43 @@ export default function ContentCard() {
                             >
                                 Github Profile
                             </ListboxItem>
-                            <ListboxItem
-                                key="resume"
-                                description="简历及项目经历"
-                                onClick={() => window.open("https://blog.neteralex.cn/resume", "_blank")}
-                                startContent={<BsFillFileRichtextFill size={22}/>}
+                        </ListboxSection>
+                        <ListboxSection title="Skill" showDivider>
+                            <ListboxItem className={"cursor-default"}
+                                         key="lang"
+                                         description="C#, TypeScript, Kotlin, Python, Dart"
+                                         startContent={<BsCodeSlash size={22}/>}
                             >
-                                Resume
+                                语言
+                            </ListboxItem>
+                            <ListboxItem className={"cursor-default"}
+                                         key="web-fullstack"
+                                         description="React, Next.js, ASP.NET, SpringBoot"
+                                         startContent={<BsStack size={22}/>}
+                            >
+                                Web 全栈开发
                             </ListboxItem>
                             <ListboxItem
-                                key="email"
-                                description="NeterAlex@outlook.com"
-                                onClick={() => window.open("mailto:neteralex@outlook.com", "_blank")}
-                                startContent={<AiFillMail size={22}/>}
+                                key="client" className={"cursor-default"}
+                                description="Flutter, WPF, MAUI, PyQt"
+                                startContent={<BsLaptop size={22}/>}
                             >
-                                Email
+                                客户端开发
+                            </ListboxItem>
+                            <ListboxItem
+                                key="chore" className={"cursor-default"}
+                                description="GIS, PostgreSQL, Git, Docker, Nginx, ML"
+                                startContent={<BsGrid size={22}/>}
+                            >
+                                其他
                             </ListboxItem>
                         </ListboxSection>
+
                         <ListboxSection title="Team">
                             <ListboxItem
                                 key="delete"
                                 description="副队长"
-                                onClick={() => window.open("https://acmproj.neteralex.cn/", "_blank")}
+                                className={"cursor-default"}
                                 startContent={<Image
                                     alt="Album cover"
                                     className="object-cover"
