1b253032329e334577bab3a4840d55b4aec45a5c
NeterAlex <neteralex@outlook.com>
2024-10-16 21:27:03 +0800

    feat: 添加视差效果

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import './styles/App.css'
 import InfoCard from "./components/infoCard.tsx";
 import ContentCard from "./components/contentCard.tsx";
 import Footer from "./components/footer.tsx";
+import {MouseParallax} from "react-just-parallax";
 
 function App() {
 
     return (
         <>
-            <div
-                className="light p-[12px] text-foreground bg-[#F3F4F6] gap-4 flex flex-col items-center justify-center align-middle h-full w-full">
-                {/*<TextCard/>*/}
-                <InfoCard/>
-                <ContentCard/>
-                <Footer/>
-            </div>
+            <MouseParallax strength={0.01} enableOnTouchDevice>
+                <div
+                    className="scrollbar-hidden light p-[12px] text-foreground bg-[#F3F4F6] gap-4 flex flex-col items-center justify-center align-middle h-full w-full">
+                    {/*<TextCard/>*/}
+                    <InfoCard/>
+                    <ContentCard/>
+                    <Footer/>
+                </div>
+            </MouseParallax>
         </>
     )
 }
