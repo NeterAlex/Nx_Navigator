8a135d3cb6141d983ffce13fff07055e25d8aaff
NeterAlex <neteralex@outlook.com>
2024-10-17 11:10:12 +0800

    feat: 新增描述文案

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,12 @@
 export default function Footer() {
-    return (<>
-        <a className="text-gray-400 text-xs" href="https://beian.miit.gov.cn/" target="_blank">
-            黑ICP备2023004156号
-        </a>
-    </>)
+    return (
+        <div className="gap-1 flex flex-col justify-center items-center">
+            <div className="text-gray-400 text-xs">
+                Powered by React & Tailwind
+            </div>
+            <a className="text-gray-400 text-xs" href="https://beian.miit.gov.cn/" target="_blank">
+                黑ICP备2023004156号
+            </a>
+        </div>
+    )
 }
